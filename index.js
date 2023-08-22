d33378ccb6a2a60bd6cd88b02a24d0a777fe371b
Miguel xD <miguel.bezerra1@gmail.com>
2023-08-22 16:32:06 -0300

    Update index.js
    Arrumei algumas frases para ficar mais adequado.

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ function calculate() {
     var imc = weight / height ** 2;
     var text=""
     if (imc < 18.5) {
-      text="Você está magro"
+      text="Você está magro, necessário ir ao nutricionista."
     } else if (imc < 24.9) {
-      text="Você está normal"
+      text="Você está dentro dos conformes saudáveis."
     } else if (imc < 29.9) {
-      text="Você está com sobrepeso"
+      text="Você está com sobrepeso."
     } else if (imc < 39.9) {
-      text="Você está com obesidade"
+      text="Você está com obesidade."
     } else if (imc > 39.9) {
-      text="Você está com obesidade mórbida"
+      text="Você está com obesidade mórbida, ir ao nutricionista urgente."
     }
     document.getElementById("text_area").innerText=text
   }
